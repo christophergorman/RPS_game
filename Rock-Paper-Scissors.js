097ed65f935dde3fc3a22f09d48b20b10a8851de
Christopher Gorman <cgorman990@gmail.com>
2023-03-16 23:27:04 -0700

    Edit .html, .css, .js files to make game work on click

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,43 +1,61 @@
-let myArray = ["Rock", "Paper", "Scissors"];
+const playerText = document.querySelector('#playerText');
+const computerText = document.querySelector('#computerText');
+const resultText = document.querySelector('#resultText');
+const playerWins = document.querySelector('#playerScore');
+const CPUWins = document.querySelector('#computerScore');
+const choiceBtns = document.querySelectorAll('.choiceBtn');
+let player;
+let computer;
+let result;
 let playerScore = 0;
-let cpuScore = 0;
-function getComputerChoice() {
-    return myArray[~~(Math.random() * myArray.length)].toLowerCase();
-}
+let CPUScore = 0;
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt("Enter Rock, Paper, or Scissors").toLowerCase();
-    computerSelection = getComputerChoice();
-    if( playerSelection == computerSelection) {
-        return("It's a Tie!");
-    } else if(( playerSelection == "Rock" && computerSelection == "Paper") ||
-        (playerSelection == "paper" && computerSelection == "scissors") || 
-        ( playerSelection == "scissors" && computerSelection == "rock")) {
-            cpuScore += 1;
-            return("You lost this round.");
-        } else {
-            playerScore += 1;
-           return("You win this round.");
-        }
-}
+choiceBtns.forEach(button => button.addEventListener('click', () => {
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    playerWins.textContent = `Number of Player Wins: ${playerScore}`;
+    CPUWins.textContent = `Number of CPU Wins: ${CPUScore}`;
+    resultText.textContent = Winner();
+    checkWinner();
+}));
 
-function game(){
-    for(let i = 0; i < 5; i++) {
-        playRound();
+function computerTurn(){
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum) {
+        case 1:
+            computer = 'ROCK';
+            break;
+        case 2:
+            computer = "PAPER";
+            break;
+        case 3:
+            computer = "SCISSORS";
+            break;
     }
 }
-function winner(){
-    if(playerScore > cpuScore) {
-        console.log("Player wins!");
-    } else if(cpuScore > playerScore){
-        console.log("CPU wins!");
-    } else {
-        console.log("It ended with a tie.");
+function Winner(){
+    if(playerScore == 5) {
+        return 'You Win!';
+    }
+    else if(CPUScore == 5) {
+        return 'You Lose!';
     }
 }
-game();
 
-console.log("Player Won: " + playerScore);
-console.log("CPU won: " + cpuScore);
-
-winner();
+function checkWinner() {
+    if(player == computer) {
+        return 'Draw!';
+    }
+    else if(computer == 'ROCK'){
+        return player == 'PAPER' ?  playerScore++ :  CPUScore++
+    }
+    else if(computer == 'PAPER'){
+        return player == 'SCISSORS' ?  playerScore++ :  CPUScore++
+    }
+    else if(computer == 'SCISSORS'){
+        return player == 'ROCK' ?  playerScore++ :  CPUScore++
+    }
+}
